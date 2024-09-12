7a88eacfaaeffc5b920fb07746a8d49650d81c8e
Tyuiu.vaganovma@outlook.com <mvaganov2006@gmail.com>
2024-09-12 23:58:48 +0500

    Task1

---
+++ Tyuiu.VaganovMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VaganovMA.Sprint0.Task1.V0;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Лисенок";
+        int age = 17;
+        string you = "люблю♥♥♥:3";
+        Console.WriteLine("Мою любимую зовут " + name + " ей " + age + " годиков." +"Я ее очень сильно"+ you);
+        Console.ReadKey();
+    }
+}
